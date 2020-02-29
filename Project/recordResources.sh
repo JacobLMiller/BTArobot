49d4a1772f33a843de8c61274b509324d652685e
Mickey253 <jacob_l._miller@hotmail.com>
2020-02-29 15:07:08 -0600

    Adding some data

--- Project/recordResources.sh
+++ Project/recordResources.sh
@@ -2,31 +2,31 @@
 echo "Process start at:"
 date
 
-listOfPIDs=$(pgrep firefox)
-#listOfPIDs=$(pgrep -d , -f turtlebot3_slam_gmapping)
+#listOfPIDs=$(pgrep firefox)
+listOfPIDs=$(pgrep -d , -f turtlebot3_slam_gmapping)
 #listOfPIDs="12345, 33333, 44444"
-#otherList=$(pgrep -d , -f -n turtlebot3_gazebo)
-otherList=$(pgrep firefox)
+otherList=$(pgrep -d , -f -n turtlebot3_gazebo)
+#otherList=$(pgrep firefox)
+
+i=0
 
 working=true
 while($working) do
 
-  i=$((i+1))
   ((i=i+1))
-  let "i=i+1"
-  sleep 5
+  sleep 2
 
-  psrecord $listOfPIDs --interval 1 --duration 20 --plot Aplot${i}1.png &
+  psrecord $listOfPIDs --interval 1 --duration 120 --log dir_out/Alog${i}.txt --plot dir_out/Aplot${i}.png &
   P1=$!
-  psrecord $otherList --interval 1 --duration 20 --plot Bplot${i}2.png &
+  psrecord $otherList --interval 1 --duration 120 --log dir_out/Blog${i}.txt --plot dir_out/Bplot${i}.png &
   P2=$!
   wait $P1 $P2
 
   #rosrun map_server map_saver -f ~/map${i}
 
-  if [ $i -ge 5 ]; then
+  if [ $i -ge 30 ]; then
     working=false
-    echo "I have successfully counted to 5."
+    echo "I'm gettting out of the loop."
   fi
 done
 
